b69d776ba1c1f5e41e380d4e0a7ba68ed57be197
Mbigeard06 <mateo.bd@icloud.com>
2023-12-06 10:21:29 +0100

    [1.2] Mise en place de l'observateur.

---
+++ Simulator/LogicLayer/Observator/IObserver.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Observator
+{
+    /// <summary>
+    /// Interface d'bserver de l'argent de l'entreprise
+    /// </summary>
+    public interface IObserver
+    { 
+        ///
+        public void MoneyChange(int money);
+    }
+}
---
+++ Simulator/LogicLayer/Observator/Subject.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicLayer.Observator
+{
+    /// <summary>
+    /// Subject class of the observator
+    /// </summary>
+    public class Subject
+    {
+        private List<IObserver> observers;
+        public Subject() 
+        {
+            observers = new List<IObserver>();
+        }
+        /// <summary>
+        /// Register a new observer
+        /// </summary>
+        /// <param name="observer"></param>
+        public void Register(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+        /// <summary>
+        /// Unregistred an observer.
+        /// </summary>
+        /// <param name="observer"></param>
+        public void Unregister(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+
+        /// <summary>
+        /// Notify the observator that the corporate money has changed.
+        /// </summary>
+        /// <param name="money"></param>
+        protected void NotifyMoneyChange(int money)
+        {
+            foreach (IObserver observer in observers)
+            {
+                observer.MoneyChange(money);
+            }
+        }
+    }
+}
